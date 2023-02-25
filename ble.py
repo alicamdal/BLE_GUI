0385328bee8ce12cbd8ef36f0cb198f1b969074b
Ali Camdal <alicamdal0@gmail.com>
2023-02-25 16:44:34 +0300

    update

--- ble.py
+++ ble.py
@@ -26,14 +26,16 @@ class Ui(QtWidgets.QMainWindow):
         self.client = None
         self.BLEOut = None
         self.BLEOut_old = None
-        self.triangle_green_up = QPixmap("assets/triangle_green_up.png")
-        self.triangle_gray_up = QPixmap("assets/triangle_gray_up.png")
-        self.triangle_green_right = QPixmap("assets/triangle_green_right.png")
-        self.triangle_gray_right = QPixmap("assets/triangle_gray_right.png")
-        self.triangle_green_left = QPixmap("assets/triangle_green_left.png")
-        self.triangle_gray_left = QPixmap("assets/triangle_gray_left.png")
-        self.triangle_green_down = QPixmap("assets/triangle_green_down.png")
-        self.triangle_gray_down = QPixmap("assets/triangle_gray_down.png")
+        self.pixMaps = {
+            "gray_up"     : QPixmap("assets/triangle_gray_up.png"),
+            "green_up"    : QPixmap("assets/triangle_green_up.png"),
+            "gray_down"   : QPixmap("assets/triangle_gray_down.png"),
+            "green_down"  : QPixmap("assets/triangle_green_down.png"),
+            "gray_right"  : QPixmap("assets/triangle_gray_right.png"),
+            "green_right" : QPixmap("assets/triangle_green_right.png"),
+            "gray_left"   : QPixmap("assets/triangle_gray_left.png"),
+            "green_left"  : QPixmap("assets/triangle_green_left.png")
+        }
         self.lblConStats.setText("Disconnected")
         self.lblConStats.setStyleSheet("background-color: red")
         self.data_queue = Queue()
@@ -71,35 +73,35 @@ class Ui(QtWidgets.QMainWindow):
         if event.key() == Qt.Key_D:
             print("Right")
             self.BLEOut = "r3"
-            self.img_right.setPixmap(self.triangle_green_right)
+            self.img_right.setPixmap(self.pixMaps["green_right"])
         elif event.key() == Qt.Key_A:
             print("Left")
             self.BLEOut = "r2"
-            self.img_left.setPixmap(self.triangle_green_left)
+            self.img_left.setPixmap(self.pixMaps["green_left"])
         elif event.key() == Qt.Key_W:
             print("Up")
             self.BLEOut = "r0"
-            self.img_up.setPixmap(self.triangle_green_up)
+            self.img_up.setPixmap(self.pixMaps["green_up"])
         elif event.key() == Qt.Key_S:
             print("Down")
             self.BLEOut = "r1"
-            self.img_down.setPixmap(self.triangle_green_down)
+            self.img_down.setPixmap(self.pixMaps["green_down"])
 
 
     def keyReleaseEvent(self, event) -> None:
         if not event.isAutoRepeat():    
             if event.key() == Qt.Key_D:
                 print("Right Released")
-                self.img_right.setPixmap(self.triangle_gray_right)
+                self.img_right.setPixmap(self.pixMaps["gray_right"])
             elif event.key() == Qt.Key_A:
                 print("Left Released")
-                self.img_left.setPixmap(self.triangle_gray_left)
+                self.img_left.setPixmap(self.pixMaps["gray_left"])
             elif event.key() == Qt.Key_W:
                 print("Up Released")
-                self.img_up.setPixmap(self.triangle_gray_up)
+                self.img_up.setPixmap(self.pixMaps["gray_up"])
             elif event.key() == Qt.Key_S:
                 print("Down Released")
-                self.img_down.setPixmap(self.triangle_gray_down)
+                self.img_down.setPixmap(self.pixMaps["gray_down"])
             self.BLEOut = "r4"
         
     async def scanner(self) -> None:
